92db06eb2669d4d8cdb97a0f8b25ff72b50e618c
Jake Ford <jakemford17@gmail.com>
2019-10-15 17:16:16 -0500

    Added styling, began creating routes

--- src/components/Forwards/Forwards.js
+++ src/components/Forwards/Forwards.js
@@ -5,14 +5,13 @@ import React from 'react';
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const AboutPage = () => (
+const Forwards = () => (
   <div>
-    <div>
-      <p>
-        This about page is for anyone to read!
-      </p>
-    </div>
+    <h1>Forwards</h1>
+    <p>
+      Forwards will go here...
+    </p>
   </div>
 );
 
-export default AboutPage;
+export default Forwards;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+
+// This is one of our simplest components
+// It doesn't have local state, so it can be a function component.
+// It doesn't dispatch any redux actions or display any part of redux state
+// or even care what the redux state is, so it doesn't need 'connect()'
+
+class Home extends Component {
+  forwardClick = () => {
+    console.log("FORWARD CLICKED");
+    // this.props.history.push(`/edit/${this.props.match.params.id}`);
+  }
+
+  defenseClick = () => {
+    console.log("DEFENSE CLICKED");
+    // this.props.history.push(`/edit/${this.props.match.params.id}`);
+  }
+
+  goalieClick = () => {
+    console.log("GOALIE CLICKED");
+    // this.props.history.push(`/edit/${this.props.match.params.id}`);
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Welcome!</h1>
+          <p>
+            Welcome to Hockey Prospect DB, a site used to help track NHL prospects for next June's NHL Entry Draft!
+          </p>
+          <button className="createPosition" onClick={this.forwardClick}>CREATE FORWARD</button>
+          <button className="createPosition" onClick={this.defenseClick}>CREATE DEFENSEMAN</button>
+          <button className="createPosition" onClick={this.goalieClick}>CREATE GOALTENDER</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
