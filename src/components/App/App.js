3da3b70f9047027c179f415ebedff4bbf2b95954
Jake Ford <jakemford17@gmail.com>
2019-10-15 17:23:46 -0500

    Fixed login Route path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,7 +14,7 @@ import Footer from '../Footer/Footer';
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
 import Home from '../Home/Home';
-import UserPage from '../UserPage/UserPage';
+// import UserPage from '../UserPage/UserPage';
 import Forwards from '../Forwards/Forwards';
 
 import './App.css';
@@ -44,11 +44,11 @@ class App extends Component {
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-              <ProtectedRoute
+              {/* <ProtectedRoute
                 exact
                 path="/home"
-                component={UserPage}
-              />
+                component={Home}
+              /> */}
               {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
               <ProtectedRoute
